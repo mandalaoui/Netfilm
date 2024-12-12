2b07bcf5930ce3a670cf17aa72a94545e8a52904
Maayan Malekan <maayan.malekan@gmail.com>
2024-12-12 14:09:39 +0200

    NP-57 post test

---
+++ src/Post.h
@@ -0,0 +1,20 @@
+#ifndef POST_H
+#define POST_H
+#include "ICommand.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cctype>
+
+using namespace std;
+class Post : public ICommand {
+    public:
+        void execute(string input) override; // Function that performs the action of adding user and movies to a user.
+        bool isInvalid(string input) override; // Function to validate the input string.
+        void addMoviesToUser(string user, string movies); // Function that add a movie to a user's watchlist.
+        void addUser(string user, string movies); // Function to add a new user to the users file and his movie.
+        bool isInFile(string str, ifstream& file); // Function to check if a string exists in a file
+        void checkUserList(string user, string movies); // Function that checks if the user already has a each movie.
+};
+#endif
--- src/tests_Post.cpp
+++ src/tests_Post.cpp
@@ -1,66 +1,48 @@
 #include <gtest/gtest.h>
 #include <iostream>
 #include <fstream>
-#include "Patch.h"
+#include "Post.h"
 #include "funcForTests.h"
 
 using namespace std;
-// Tests for function "Execute" in class "Patch"
+// Tests for function "Execute" in class "Post"
 
     // Test for adding a new user
-TEST(PatchExecuteTest, UserIdNotFound) {
+TEST(PostExecuteTest, UserIdNotFoundCreateNew) {
     // Initialize the "users" file (clear or create it)   
     const string inputs[] = {"1", "121", "115", "20"};
     // Loop through each input and test it
     for (int i = 0; i < sizeof(inputs); ++i) {
         setFile("users","12\n15\n20\n19\n");
         // Execute the function with each input from the array and check the response
-        ASSERT_TRUE(checkResponseFromServer("PATCH "+ inputs[i] + " 35 20", "404 Not Found"));
-        // Create a file to store the result as after patching the user
-        setFile("usersAfterPatch", "12\n15\n20\n19\n");
+        ASSERT_TRUE(checkResponseFromServer("POST "+ inputs[i] + " 35 20", "201 Created"));
+        // Create a file to store the result as after adding the user
+        setFile("usersAfterAdd", "12\n15\n20\n19\n" + inputs[i]);
         // Compare the "before" and "after" files to validate the change
         ASSERT_TRUE(compareFiles("users", "usersAfterPatch")) << "Comparison for " << inputs[i] << " failed!";
     }
 }
 
-    // Test for adding a new movie for existing user
-TEST(PatchExecuteTest, UserIdFoundAddingMovie) {
+    // Test for posting with existing user
+TEST(PostExecuteTest, UserIdFound) {
     // Initialize the "users" file (clear or create it)
     setFile("users", "1\n2\n3\n4\n");
-    const string inputs[] = {"11", "1001", "150", "100100"};
+    const string inputs[] = {"11", "100", "150", "103"};
     // Loop through each input and test it
     for (int i = 0; i < sizeof(inputs); ++i) {
         // Creates the watch list of user 1
         setFile("1_watchlist", "100\n101\n102\n103\n");
         // Execute the function with each input from the array and check the response
-        ASSERT_TRUE(checkResponseFromServer("PATCH 1 "+ inputs[i], "204 No Content"));
+        ASSERT_TRUE(checkResponseFromServer("PATCH 1 "+ inputs[i], "404 Not Found"));
         // Create a file to store the result as after adding the movie
-        setFile("watchListAfterAdd", "100\n101\n102\n103\n" + inputs[i] +"\n");
+        setFile("watchListAfterAdd", "100\n101\n102\n103\n");
         // Compare the "before" and "after" files to validate the change
         ASSERT_TRUE(compareFiles("1_watchlist", "watchListAfterAdd")) << "Comparison for " << inputs[i] << " failed!";
     }
 }
 
-    // Test for not adding a movie that already exist
-TEST(PatchExecuteTest, UserIdFoundMovieExists) {
-    // Initialize the "users" file (clear or create it)
-    setFile("users", "1\n2\n3\n4\n");
-    // Creates the watch list of user 1
-    setFile("1_watchlist", "100\n101\n102\n103\n");
-    const string inputs[] = {"100", "101", "102", "103"};
-    // Loop through each input and test it
-    for (int i = 0; i < sizeof(inputs); ++i) {
-        // Execute the function with each input from the array and check the response
-        ASSERT_TRUE(checkResponseFromServer("PATCH 1 "+ inputs[i], "404 Not Found"));
-        // Create a file to store the previous watchlist
-        setFile("watchListAfterAdd", "100\n101\n102\n103\n");
-        // Compare the "before" and "after" files to validate there are no changes - not suppose to add any movie
-        compareFiles("1_watchlist", "watchListAfterAdd");
-    }
-}
-
     // Test for not adding a movie that was repeated
-TEST(PatchExecuteTest, UserIdFoundMovieRepeats) {
+TEST(PostExecuteTest, UserIdNotFoundMovieRepeats) {
     // Initialize the "users" file (clear or create it)
     setFile("users", "1\n2\n3\n4\n");
     const string inputs[] = {"104", "105"};
@@ -69,7 +51,7 @@ TEST(PatchExecuteTest, UserIdFoundMovieRepeats) {
         // Creates the watch list of user 1
         setFile("1_watchlist", "100\n101\n102\n103\n");
         // Execute the function with each input from the array and check the response
-        ASSERT_TRUE(checkResponseFromServer("PATCH 1 150 " + inputs[i] + " " + inputs[i], "204 No Content"));
+        ASSERT_TRUE(checkResponseFromServer("POST 1 150 " + inputs[i] + " " + inputs[i], "201 Created"));
         // Create a file to store the the result as after adding the correct movies
         setFile("watchListAfterAdd", "100\n101\n102\n103\n150\n" + inputs[i] + "\n");
         // Compare the "correct" and "tested" files to validate the difference - suppose to add just one each time (In addition to the 150 id mocie)
@@ -78,21 +60,17 @@ TEST(PatchExecuteTest, UserIdFoundMovieRepeats) {
 }
 
     // Test for valid input some spaces - suppose to work normally
-TEST(PatchExecuteTest, ValidInputWithSpaces) {
+TEST(PostExecuteTest, ValidInputWithSpaces) {
     // Initialize the "users" file (clear or create it)
-    setFile("users", "1\n2\n3\n4\n");
-    // Creates the watch list of user 1
-    setFile("1_watchlist", "100\n101\n102\n103\n");
-    // Creates a copy of the watch list of user 1
-    duplicateFile("1_watchlist", "2_watchlist");
-    // Execute the function with each file and similar input and check the response
-    ASSERT_TRUE(checkResponseFromServer("PATCH 1 150 160 1600", "204 No Content"));
-    ASSERT_TRUE(checkResponseFromServer("PATCH 2 150   160   1600", "204 No Content"));
+    setFile("users", "1\n2\n");
+    // Execute the function with each user and similar movies and check the response
+    ASSERT_TRUE(checkResponseFromServer("POST 3 150 160 1600", "201 Created"));
+    ASSERT_TRUE(checkResponseFromServer("POST 4 150   160   1600", "201 Created"));
     // Compare the files to validate the changes - suppose to behave the same
-    ASSERT_TRUE(compareFiles("1_watchlist", "2_watchlist")) << "Comparison for spaces failed!";
+    ASSERT_TRUE(compareFiles("3_watchlist", "4_watchlist")) << "Comparison for spaces failed!";
 }
 
-TEST(AddExecuteTest, invalidInputs) {
+TEST(PostExecuteTest, invalidInputs) {
      // Initialize the "users" file (clear or create it)
     setFile("users", "1\n2\n3\n4\n");
     // Creates the watch list of user 1
@@ -104,7 +82,7 @@ TEST(AddExecuteTest, invalidInputs) {
     // Loop through each input and test it
     for (int i = 0; i < sizeof(invalidInputs); ++i) {
         // Execute the function with each input from the array and check the response
-        ASSERT_TRUE(checkResponseFromServer("PATCH invalidInputs[i]", "400 Bad Request"));
+        ASSERT_TRUE(checkResponseFromServer("POST invalidInputs[i]", "400 Bad Request"));
         // Compare the "before" and "after" files to validate there are no changes - not suppose to add anything
         ASSERT_TRUE(compareFiles("1_watchlist", "2_watchlist")) << "Comparison for " << invalidInputs[i] << " failed!";
     }
