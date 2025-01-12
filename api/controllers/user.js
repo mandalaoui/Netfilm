28594ce4021b7e24d7cb275db81151a34183b134
mandalaoui <omeremomer2000@gmail.com>
2025-01-12 23:50:53 +0200

    compeltion of edge cases

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -5,17 +5,16 @@ const createUser = async (req, res) => {
     const newUser = await userService.createUser(req.body.username, req.body.email, req.body.password,
         req.body.age, req.body.phoneNumber, req.body.photo, req.body.nickname, req.body.creditcard, req.body.watchedMovies);
     const location = `/api/users/${newUser._id}`;
-    res.status(201).location(location).json(newUser);
+    res.status(201).location(location).json();
 };
 
 // Retrieves a user by ID. Responds with 404 if the user is not found, otherwise returns the user as JSON.
-const getUser = async (req, res, next) => {
+const getUser = async (req, res) => {
     const user = await userService.getUserById(req.params.id);
     if (!user) {
         return res.status(404).json({ errors: ['User not found'] });
     }
     res.status(200).json(user);
-    next();
 };
 
 // Exports the createUser and getUser functions.
